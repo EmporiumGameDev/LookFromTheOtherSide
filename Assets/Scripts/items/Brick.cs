d2b021a2879bf1c25d723ad45b6954bfb0b6dfeb
crimpik <romanklimentev62723@gmail.com>
2023-12-15 20:19:55 +0300

    BugBrick

--- Assets/Scripts/items/Brick.cs
+++ Assets/Scripts/items/Brick.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Brick : Item
 {
-    
+    public Vector3 Velocity;
+    public Rigidbody RidgedBody;
+    public bool IsAlive = true;
+    private double _decelerationTolerance = 6.0;
+
+    void Start()
+    {
+        RidgedBody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+    }
 }

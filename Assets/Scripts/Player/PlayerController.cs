860e4cb43274bf8c6999311b0ad28ce2b41c4225
crimpik <romanklimentev62723@gmail.com>
2023-12-17 01:47:10 +0300

    GordoThePlayer

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,10 +12,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _movemenetSpeed;
     [SerializeField] private float _jumpForce;
 
+    [SerializeField] private Animator _animator;
+
     private PlayerInputActions _playeInputActions;
     private Vector2 _horizontal;
     private bool _isFacingRight = true;
     private bool _isGrounded = true;
+    private bool _isRun = true;
 
     private void OnEnable()
     {
@@ -41,14 +44,29 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
+        
         _isGrounded = Physics.Raycast(transform.position, Vector3.down, 1f, _layerMask);
 
         if (context.performed && _isGrounded)
+        {
             _rb.AddForce(0, _jumpForce, 0, ForceMode.Impulse);
+            _animator.SetTrigger("Jump_01");
+        }
+
     }
 
     private void Move()
     {
+        if(_horizontal.x == 0 ) 
+        {
+            _isRun = false;
+            _animator.SetTrigger("Idle_01");
+        }
+        else if(!_isRun)
+        {
+            _isRun = true;
+            _animator.SetTrigger("Run_01");
+        }
         _rb.velocity = new Vector3(_horizontal.x * _movemenetSpeed, _rb.velocity.y, _rb.velocity.z);
     }
 

10ab8188cb2e86355a5c359199079954237844a1
MIkhail Khorokhorin <mikhailkhorokhorin@gmail.com>
2023-12-16 13:51:59 +0300

    Create Scene Switch Mechanic

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void Home() => SceneManager.LoadScene(0);
+
+    public void LoadLevel() => SceneManager.LoadScene(Progress.Instance.Level + 1);
+}
